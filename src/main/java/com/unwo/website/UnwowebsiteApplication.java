11b03e084be189479f79116bf5029379ecd4b200
Allen Tan <wacr.thj@qq.com>
2023-06-02 10:01:53 +0800

    Initial commit

---
+++ src/main/java/com/unwo/website/UnwowebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.unwo.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnwowebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnwowebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unwo/website/UnwowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unwo.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnwowebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
