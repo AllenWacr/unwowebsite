277b0480764d0a89f1beddb1d46641ca21050818
Allen Tan <wacr.thj@qq.com>
2023-06-06 17:01:33 +0800

    user controller

--- src/main/java/com/unwo/website/controller/UserController.java
+++ src/main/java/com/unwo/website/controller/UserController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.unwo.website.bean.User;
 import com.unwo.website.service.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -42,13 +45,12 @@ public class UserController {
     @PostMapping("/update/${flag}/${id}")
     public String updateUserDone(ModelMap map, @PathVariable String flag, @PathVariable Integer id, String update) {
         User updateUser = User.builder().id(id).build();
-        int success;
         switch(flag) {
             case "username": updateUser.setUsername(update); break;
             case "password": updateUser.setPassword(update); break;
             default: 
         }
-        success = userService.updateUser(updateUser, flag);
+        int success = userService.updateUser(updateUser, flag);
         map.addAttribute("user", userService.selectUser(updateUser, "id"));
         map.addAttribute("success", success);
         map.addAttribute("type", "update");
@@ -61,4 +63,39 @@ public class UserController {
         map.addAttribute("type", "delete");
         return "deleteUser";
     }
+
+    @PostMapping("/delete/${id}")
+    public String deleteUserDone(ModelMap map, @PathVariable Integer id) {
+        int success = userService.deleteUser(User.builder().id(id).build());
+        map.addAttribute("success", success);
+        map.addAttribute("type", "delete");
+        return "deleteUser";
+    }
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, ModelMap map) {
+        map.addAttribute("type", "login");
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginDone(HttpServletRequest request, ModelMap map, String username, String password) {
+        int success = userService.login(User.builder().username(username).password(password).build());
+        if(success == 0) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("username", username);
+        }
+        map.addAttribute("success", success);
+        map.addAttribute("type", "login");
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(ModelMap map, HttpSession session) {
+        session.removeAttribute("username");
+        map.addAttribute("type", "logout");
+        return "logout";
+    }
+
+    
 }
